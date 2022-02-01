89966d2440334279f866d52791451c7dae0b53bc
pghosh <parthaghosh.iitbbs@gmail.com>
2022-02-01 12:15:56 +0100

    cluster running code fix

--- scripts/utils/prepare_data.sh
+++ scripts/utils/prepare_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python make_hdf5.py --dataset I128 --batch_size 256 --data_root '/is/cluster/pghosh/'
-python calculate_inception_moments.py --dataset I128_hdf5 --data_root 'is/cluster/pghosh/'
+python make_hdf5.py --dataset I128 --batch_size 256 --data_root '/is/cluster/work/pghosh/datasets/imagenet_2012/ILSVRC/Data/CLS-LOC'
+python calculate_inception_moments.py --dataset I128_hdf5 --data_root '/is/cluster/work/pghosh/datasets/imagenet_2012/ILSVRC/Data/CLS-LOC'
